41a9229a1f843a5c1363d597562e3015633ab344
zeususerbot <50679858+zeususerbot@users.noreply.github.com>
2019-05-23 02:53:04 +0530

    Update boobs.py

--- IHbot/modules/boobs.py
+++ IHbot/modules/boobs.py
@@ -17,8 +17,8 @@ from IHbot.__main__ import STATS, USER_INFO
 from IHbot.modules.disable import DisableAbleCommandHandler
 
 def boobs(bot: Bot, update: Update):
-    nsfw = requests.get('http://api.odicks.ru/noise/1').json()[0]["preview"]
-    final = "http://media.odicks.ru/{}".format(nsfw)
+    nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
+    final = "http://media.oboobs.ru/{}".format(nsfw)
     update.message.reply_photo(final)
 
 BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs)
